f729c98a09460ef46c604d06b90ace3571b05590
shunsyun <shunsukesawai0422@gmail.com>
2019-06-19 19:01:40 +0900

    summry

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users,:controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+  devise_for :users,controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
   } 
   devise_scope :user do
-    get "sign_up", :to => "users/registrations#new"
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
+    get "sign_up", to: "users/registrations#new"
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy" 
    
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "items#index"
-  get "items/delete" => "items#delete"
+  get "items/delete",to: "items#delete"
   resources :wallets, only: [:new]
   resources :items, only:[:index, :show]
   resources :users, only:[:index, :show, :new] do
