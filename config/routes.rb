91d2870ba36f703b9a95babba9303f6e7f66dd52
kenta-matsuzaki <kenta6910@gmail.com>
2019-06-20 13:01:06 +0900

    Summary(required)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "items#index"
+  get "users/identification", to: "users#identification", as: "identification"
+
   resources :wallets, only: [:new]
   resources :items, only:[:index]
   get "items/delete" => "items#delete"
