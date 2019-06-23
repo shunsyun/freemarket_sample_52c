dd02706bcc215efe3526e581a1905d657c61afdf
shunsyun <shunsukesawai0422@gmail.com>
2019-06-23 18:48:53 +0900

    create item-index-function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
-  def index
-  end
+  # before_action :set_category
 
+  def index
+    @item = Item.new
+    @ladies = Item.order('id DESC').limit(4).where(category_id:1)
+    @mens = Item.order('id DESC').limit(4).where(category_id:2)
+    @babies = Item.order('id DESC').limit(4).where(category_id:3)
+    @cosmes =Item.order('id DESC').limit(4).where(category_id:4)
+    @chanels =Item.order('id DESC').limit(4).where(category_id:5)
+    @louis =Item.order('id DESC').limit(4).where(category_id:6)
+    @supremes =Item.order('id DESC').limit(4).where(category_id:7)
+    @nikes =Item.order('id DESC').limit(4).where(category_id:8)
+  end
   # def delete
   # end
 
@@ -15,6 +25,15 @@ class ItemsController < ApplicationController
   end
 
   def buy
+  end
+
+  private
+  def create_params
+    params.require(:item).permit(:name,:price,:description,:status).merge(image_id: params[:image_id])
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
   end
 
 end
