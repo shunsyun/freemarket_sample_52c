60e507b15c71e49395f9f6485aa94233c08b04dd
Ryohei Koga <z52ryoheikoga@gmail.com>
2019-06-25 19:39:57 +0900

    ロールバック6/25

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
+
 class ItemsController < ApplicationController
+  # before_action :set_category
+
   def index
+    @item = Item.new
     @ladies = Item.recent.where(category_id:1)
     @mens = Item.recent.where(category_id:2)
     @babies = Item.recent.where(category_id:3)
     @cosmes =Item.recent.where(category_id:4)
-    @chanels =Item.recent.where(category_id:5)
-    @louis =Item.recent.where(category_id:6)
-    @supremes =Item.recent.where(category_id:7)
-    @nikes =Item.recent.where(category_id:8)
+    @chanels =Item.recent.where(brand_id:1)
+    @louis =Item.recent.where(brand_id:2)
+    @supremes =Item.recent.where(brand_id:3)
+    @nikes =Item.recent.where(brand_id:4)
   end
 
   def delete
@@ -37,6 +41,10 @@ class ItemsController < ApplicationController
   private
   def create_params
     params.require(:item).permit(:name,:price,:description,:status).merge(image_id: params[:image_id])
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
   end
 
 end
