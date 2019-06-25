22dfa351bf908dab50cbea1ccfeca8f4461183d2
tomohironagano <t0mo.0724@icloud.com>
2019-06-25 18:14:58 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,27 +12,24 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "items#index"
+  get "users/delete",to: "users#delete"
+  get "items/buy",to: "items#buy"
+  get "items/buy_done",to: "items#buy_done"
+  post "items/pay",to: "items#pay"
+  get "users/identification", to: "users#identification", as: "identification"
+  get "mypage", to: "users#mypage", as: "mypage"
   resources :wallets, only: [:new]
-  resources :items, only:[:index, :show] do
-    collection do
-      get "buy"
-      get "buy_done"
-      get "sell", to: "items#sell", as: "sell"
-      get "mypage/profile", to:"items#mypage_profile", as: "mypage/profile"
-      post "pay"
-    end
-end
+  resources :items, only:[:index, :show]
   resources :users, only:[:index, :show, :new] do
   collection do
     get "number"
     get "address"
     get "done"
     get "credit"
-    get "delete"
-    get "mypage", to: "users#mypage", as: "mypage"
-    get "identification", to: "users#identification", as: "identification"
   end
 end
+  get "mypage/profile", to:"items#mypage_profile", as: "mypage/profile"
+  get "sell", to: "items#sell", as: "sell"
   
 end
 
