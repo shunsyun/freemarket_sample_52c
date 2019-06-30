abcab7dc604d829e105b7fed822c9a2d8e65a20f
tomohironagano <t0mo.0724@icloud.com>
2019-06-30 14:35:40 +0900

    fix migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_092416) do
 ActiveRecord::Schema.define(version: 2019_06_29_051904) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -72,8 +71,8 @@ ActiveRecord::Schema.define(version: 2019_06_29_051904) do
     t.datetime "updated_at", null: false
     t.bigint "image_id"
     t.bigint "category_id"
-    t.integer "prefecture"
     t.integer "seller_id"
+    t.integer "prefecture"
     t.integer "category_l"
     t.string "brand"
     t.text "image"
