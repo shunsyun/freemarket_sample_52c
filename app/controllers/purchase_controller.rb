75786dc764c0853791c7e5a39f43c003ecbf310d
tomohironagano <t0mo.0724@icloud.com>
2019-07-01 20:14:31 +0900

    条件分岐

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -35,7 +35,8 @@ class PurchaseController < ApplicationController
     customer: card.customer_id, #顧客ID
     currency: 'jpy', #日本円
   )
-    @item.buyer_id = 1
+    @item.buyer_id = current_user.id
+    @item.sales_status = "売却済み"
     @item.save
   redirect_to action: 'done' #完了画面に移動
   end
